43457db7af35d4c0f05145650ccab38d427dbd32
maureendugan <maureen.k.dugan@gmail.com>
2016-04-23 16:42:54 -0700

    Refactor to StartApp

--- PokerClock.elm
+++ PokerClock.elm
@@ -1,13 +1,24 @@
 module PokerClock where
 
 import Html exposing (..)
+import StartApp.Simple as StartApp
 
 
-main : Html
 main =
+  StartApp.start { model = model, view = view, update = update }
+
+
+model = ""
+
+
+view address model =
   div []
-    [ h1 [] [text "Poker Clock"]
-    , h2 [] [text "00:00"]
-    , button [] [text "Play"]
-    , button [] [text "Pause"]
+    [ h1 [] [ text "Poker Clock" ]
+    , h2 [] [ text "00:00" ]
+    , button [] [ text "Play" ]
+    , button [] [ text "Pause" ]
     ]
+
+
+update action model =
+  model
