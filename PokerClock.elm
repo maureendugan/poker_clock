93f65c11e8d0cd0988be9e9203cdddc7efa75aa6
maureendugan <maureen.k.dugan@gmail.com>
2016-04-23 17:45:05 -0700

    Add inputs with Signals and blow your mind a bit

--- PokerClock.elm
+++ PokerClock.elm
@@ -3,10 +3,11 @@ module PokerClock where
 import Effects
 import Html exposing (..)
 import StartApp
+import Time exposing ( every, second )
 
 
 app =
-  StartApp.start { init = init, view = view, update = update, inputs = [] }
+  StartApp.start { init = init, view = view, update = update, inputs = inputs }
 
 
 main =
@@ -37,3 +38,16 @@ update action model =
 formatSeconds : Int -> String
 formatSeconds seconds =
   toString seconds
+
+
+inputs : List (Signal Action)
+inputs =
+  [ Signal.map countdown (every second) ]
+
+
+countdown : a -> Action
+countdown _ =
+  Decrement
+
+
+type Action = Decrement | Noop
