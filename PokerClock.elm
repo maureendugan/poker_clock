40ce4fe6ee8ca0257c1c857aaac4b55f3437139e
Micah Geisel <micah@botandrose.com>
2016-04-28 01:08:17 -0700

    WIP

--- PokerClock.elm
+++ PokerClock.elm
@@ -1,5 +1,6 @@
 module PokerClock where
 
+import Audio
 import Effects
 import Html exposing (..)
 import Html.Events exposing (..)
@@ -8,14 +9,17 @@ import String
 import Time exposing ( every, second )
 
 
+app : StartApp.App Model
 app =
   StartApp.start { init = init, view = view, update = update, inputs = inputs }
 
 
+main : Signal Html
 main =
   app.html
 
 
+init : ( Model, Effects.Effects Action )
 init =
   ( initialModel, Effects.none )
 
@@ -24,11 +28,12 @@ type alias Model = { counter : Int, isPaused : Bool }
 
 initialModel : Model
 initialModel =
-  { counter = 900
-  , isPaused = True
+  { counter = 10
+  , isPaused = False
   }
 
 
+view : Signal.Address Action -> Model -> Html
 view address model =
   div []
     [ h1 [] [ text "Poker Clock" ]
@@ -38,11 +43,14 @@ view address model =
     ]
 
 
-update : Action -> Model -> ( Model, Effects.Effects a )
+update : Action -> Model -> ( Model, Effects.Effects Action )
 update action model =
   let
     updatedModel =
       case action of
+        Noop ->
+          model
+
         Decrement ->
           if model.isPaused
             then model
@@ -51,8 +59,13 @@ update action model =
         Toggle ->
           { model | isPaused = not model.isPaused }
 
+    updatedEffects =
+      if model.counter % 2 == 0
+        then Signal.send audioActionMailbox.address Audio.Play |> Effects.task |> Effects.map (always Noop)
+        else Effects.none
+
   in
-    ( updatedModel, Effects.none )
+    ( updatedModel, updatedEffects )
 
 
 formatSeconds : Int -> String
@@ -72,10 +85,28 @@ formatSeconds seconds =
 
 inputs : List (Signal Action)
 inputs =
-  [ Signal.map (always Decrement) (every second) ]
+  [ Signal.map (always Decrement) (every second)
+  , Signal.map (\s -> always Noop (Debug.log "omg" s)) audioSignal
+  ]
 
 
-type Action = Decrement | Toggle
+type Action = Decrement | Toggle | Noop
+
+
+audioActionMailbox : Signal.Mailbox Audio.Action
+audioActionMailbox =
+  Signal.mailbox Audio.NoChange
+
+
+audioSignal : Signal ( Audio.Event, Audio.Properties )
+audioSignal =
+  Audio.audio
+    { src = "snd/theme.mp3"
+    , triggers = { timeupdate = True, ended = True }
+    , propertiesHandler = always Nothing
+    , actions = audioActionMailbox.signal
+    }
+
 
 -- To Dos: 
 -- 1) Add logic to handle model = 0; It should make a noise, hold at zero
