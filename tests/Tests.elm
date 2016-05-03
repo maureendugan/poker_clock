92ddf7b76e0d3ee6a8d2390fa8ac64e4592f7c4c
Micah Geisel <micah@botandrose.com>
2016-05-02 20:48:59 -0700

    WIP

---
+++ tests/Tests.elm
@@ -0,0 +1,21 @@
+module Tests where
+
+import ElmTest exposing (..)
+import PokerClock
+import String
+
+
+all : Test
+all =
+  suite "A Test Suite"
+    [ test "Addition" <|
+      assertEqual (3 + 7) 10
+
+    , test "String.left" <|
+      assertEqual "a" <| String.left 1 "abcdefg"
+
+    , test "This test should fail" <|
+      assert True
+
+    ]
+
