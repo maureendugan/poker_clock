41d839f3fd053eff468129148a4e37d5569cfdd2
Maureen Dugan <maureen.k.dugan@gmail.com>
2016-05-04 00:17:27 -0700

    Add beep (#4)
    * Implement a port that recieves a signal to play a
    beep. This requires a mailbox - which gives the
    port an address and a default value, a task that
    sends a different value to the address, an effect
    on the model, and a bookend function to turn a
    tuple into an Action.
    
    * Use a temp variable, like a private method in the action, to clean up conditional
    
    * Change Decrement to Tick and clamp the model to a particular range
    
    * New beep and removed buttons for MVP

--- PokerClock.elm
+++ PokerClock.elm
@@ -3,11 +3,16 @@ module PokerClock where
 import Effects
 import Html exposing (..)
 import StartApp
+import Task exposing (Task)
 import Time exposing ( every, second )
 
 
 app =
   StartApp.start { init = init, view = view, update = update, inputs = inputs }
+
+
+port tasks : Signal (Task Effects.Never ())
+port tasks = app.tasks
 
 
 main =
@@ -26,15 +31,23 @@ view address model =
   div []
     [ h1 [] [ text "Poker Clock" ]
     , h2 [] [ text (formatSeconds model) ]
-    , button [] [ text "Play" ]
-    , button [] [ text "Pause" ]
+    -- , button [] [ text "Play" ]
+    -- , button [] [ text "Pause" ]
     ]
 
 
-update : Action -> Model -> ( Model, Effects.Effects a )
+update : Action -> Model -> ( Model, Effects.Effects Action )
 update action model =
-  case action of
-    Decrement -> ( model - 1, Effects.none )
+  let
+    playBeepIfZero =
+      if model - 1 == 0
+        then playBeepEffect
+        else Effects.none
+
+  in
+    case action of
+      Tick -> ( clamp 0 900 (model - 1), playBeepIfZero )
+      Noop -> ( model, Effects.none )
 
 
 formatSeconds : Int -> String
@@ -44,14 +57,35 @@ formatSeconds seconds =
 
 inputs : List (Signal Action)
 inputs =
-  [ Signal.map (always Decrement) (every second) ]
+  [ Signal.map (always Tick) (every second) ]
 
 
-type Action = Decrement
+type Action = Tick | Noop
+
 
 type alias Model = Int
 
--- To Dos: 
+
+port playBeep : Signal Bool
+port playBeep =
+  playBeepMailbox.signal
+
+
+playBeepMailbox : Signal.Mailbox Bool
+playBeepMailbox =
+  Signal.mailbox False
+
+
+playBeepTask : Task x ()
+playBeepTask =
+  Signal.send playBeepMailbox.address True
+
+
+playBeepEffect : Effects.Effects Action
+playBeepEffect =
+  Effects.map (always Noop) (Effects.task playBeepTask)
+
+-- To Dos:
 -- 1) Add logic to handle model = 0; It should make a noise, hold at zero
 -- 2) Implement restart/refresh button
 -- 3) Implement pause
